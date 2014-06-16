30044c9668755043408328bc7639532984fdbb40
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2014-06-16 20:34:31 +0900

    BitBucket との連携テスト

--- app.rb
+++ app.rb
@@ -15,16 +15,32 @@ get '/' do
   response
 end
 
-post '/' do
+post '/bitbucket/post' do
   puts '~~~~~ POST /'
-  puts ::MultiJson::dump(params)
+  body = params[:payload]
+  repository = "#{body[:owner]}/#{body[:name]}"
+  url = body[:canon_url] + body[:absolute_url]
+
+  str = '<%s|%s> pushed to <%s|%s>' % [
+      "#{body[:canon_url]}/#{body[:user]}",
+      body[:user],
+      repository,
+      url
+  ]
+
+#  body[:commits].each do |commit|
+#    author = commit[:author]
+#    node = commit[:node]
+#    message = commit[:message]
+#  end
+
+  Slack.chat_postMessage ({
+    token: ENV['SLACK_API_TOKEN'],
+    channel: '#gfc',
+    text: str,
+    username: 'BitBucket',
+    icon_url: 'https://slack.global.ssl.fastly.net/20653/img/services/bitbucket_48.png'
+  })
 end
 
-#Slack.chat_postMessage ({
-#  token: ENV['SLACK_API_TOKEN'],
-#  channel: '#gfc',
-#  text: '> Hello world',
-#  username: 'Ruby',
-#  icon_url: 'https://slack.global.ssl.fastly.net/10562/img/services/trello_48.png'
-#})
 
