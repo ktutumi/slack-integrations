e691357a7c4fa8d477338073c356f8cb14b2138b
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2014-06-16 19:54:57 +0900

    JSON変換テスト

--- index.rb
+++ index.rb
@@ -1,18 +1,25 @@
+require 'sinatra'
+require 'multi_json'
 require 'slack'
 
 #ENV['SLACK_API_TOKEN'] = 'xoxp-2381348639-2381348641-2387116848-200f20'
-
-puts '~~~~~ SLACK_API_TOKEN: %s' % ENV['SLACK_API_TOKEN'];
 
 Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN'];
 end
 
-Slack.chat_postMessage ({
-  token: ENV['SLACK_API_TOKEN'],
-  channel: '#gfc',
-  text: '> Hello world',
-  username: 'Ruby',
-  icon_url: 'https://slack.global.ssl.fastly.net/10562/img/services/trello_48.png'
-})
+
+get '/' do
+  response = MultiJson::dump({foo: 'bar', hoge: 'fuga'})
+  puts response
+  response
+end
+
+#Slack.chat_postMessage ({
+#  token: ENV['SLACK_API_TOKEN'],
+#  channel: '#gfc',
+#  text: '> Hello world',
+#  username: 'Ruby',
+#  icon_url: 'https://slack.global.ssl.fastly.net/10562/img/services/trello_48.png'
+#})
 
